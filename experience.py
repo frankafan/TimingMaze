f9c9b08026a3af008972c39749adfc7fb3046dd6
frankafan <frank.a.fan@outlook.com>
2024-09-16 00:14:58 -0400

    Store border in experience.

--- experience.py
+++ experience.py
@@ -5,10 +5,19 @@ class Experience:
     def __init__(self, L, r):
         self.L = L
         self.r = r
-        self.cur_pos = (0, 0)
+        self.cur_pos = (
+            0,
+            0,
+        )  # (x, y) coordinates relative to the original start position
         self.seen_cells = (
             set()
         )  # set of tuples (x, y) storing coordinates of cells relative to the original start position
+        self.walls = (
+            float("inf"),
+            float("inf"),
+            float("-inf"),
+            float("-inf"),
+        )  # (right, top, left, bottom) coordinates relative to the original start position
 
     def move(self, current_percept):
         """Update experience with new cell seen in this move
@@ -18,13 +27,56 @@ class Experience:
         """
 
         self.cur_pos = (-current_percept.start_x, -current_percept.start_y)
+
+        # initialize coordinates for the maximum field of view relative to current position
+        right, top, left, bottom = 0, 0, 0, 0
+
         for cell in current_percept.maze_state:
+
+            # update field of view coordinates relative to current position
+            right, top, left, bottom = (
+                max(right, cell[0]),
+                max(top, cell[1]),
+                min(left, cell[0]),
+                min(bottom, cell[1]),
+            )
+
             cell = (
                 cell[0] - current_percept.start_x,
                 cell[1] - current_percept.start_y,
             )
             if (cell[0], cell[1]) not in self.seen_cells:
                 self.seen_cells.add((cell[0], cell[1]))
+
+        # update walls coordinates relative to the original start position
+        if right < self.r:
+            self.walls = (
+                right + self.cur_pos[0],
+                self.walls[1],
+                self.walls[2],
+                self.walls[3],
+            )
+        if top < self.r:
+            self.walls = (
+                self.walls[0],
+                top + self.cur_pos[1],
+                self.walls[2],
+                self.walls[3],
+            )
+        if left > -self.r:
+            self.walls = (
+                self.walls[0],
+                self.walls[1],
+                left + self.cur_pos[0],
+                self.walls[3],
+            )
+        if bottom > -self.r:
+            self.walls = (
+                self.walls[0],
+                self.walls[1],
+                self.walls[2],
+                bottom + self.cur_pos[1],
+            )
 
     def get_best_move(self):
         """Get the move that maximizes the number of new cells seen
